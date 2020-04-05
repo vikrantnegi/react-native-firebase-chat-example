d11dfef41c7a02659b35b3eb26ddbce0d89ca21a
vikrantnegi <vikrantnegi007@gmail.com>
2020-04-05 14:40:16 +0530

    add react navigation and source files and folders

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import AppNavigator from './navigation/AppNavigator';
+
+export default function App() {
+  return <AppNavigator />;
+}
