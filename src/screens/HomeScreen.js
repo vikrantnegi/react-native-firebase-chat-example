5d7f6518945a365452d56437378d1b0d11671964
vikrantnegi <vikrantnegi007@gmail.com>
2020-04-05 15:03:33 +0530

    add home screen ui

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  TextInput,
+} from 'react-native';
 
 function HomeScreen() {
+  const [name, onChangeName] = useState('');
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 15,
+      }}>
+      <Text style={styles.title}>Enter your name:</Text>
+
+      <TextInput
+        placeHolder="John Doe"
+        style={styles.inputStyle}
+        onChangeText={text => onChangeName(text)}
+        value={name}
+      />
+      <TouchableOpacity style={styles.buttonWrapper} onPress={this.onPress}>
+        <Text style={styles.buttonText}>Next</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+const offset = 24;
+
+const styles = StyleSheet.create({
+  inputStyle: {
+    height: offset * 2,
+    paddingHorizontal: offset,
+    borderColor: '#111111',
+    borderWidth: 1,
+    width: '100%',
+    marginBottom: offset / 2,
+  },
+  title: {
+    fontSize: offset,
+    marginBottom: offset / 2,
+  },
+  buttonWrapper: {
+    backgroundColor: '#5e72e4',
+    paddingVertical: offset / 3,
+    paddingHorizontal: offset / 2,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: offset,
+    textAlign: 'center',
+  },
+});
 
 export default HomeScreen;
