ec86d048a4fe047b494e65a2813582b05e428357
vikrantnegi <vikrantnegi007@gmail.com>
2020-04-07 13:28:22 +0530

    update register screen

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,26 +1,58 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import {Button, TextInput} from 'react-native-paper';
 import {responsiveWidth} from 'react-native-responsive-dimensions';
+import {showMessage} from 'react-native-flash-message';
+import auth from '@react-native-firebase/auth';
+
+import FullLoading from '../components/Loader';
 
 export default function Signup({navigation}) {
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const getErrorMessage = () => {
+    if (!email && !password) {
+      return 'Fields are mandatory';
+    }
+    if (!email) {
+      return 'Email is missing';
+    }
+    if (!password) {
+      return 'Password is missing';
+    }
+  };
+
+  const register = async () => {
+    if (!email || !password) {
+      const error = getErrorMessage();
+      showMessage({
+        message: error,
+      });
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      await auth().createUserWithEmailAndPassword(email, password);
+
+      return navigation.navigate('Chat');
+    } catch (error) {
+      // stop loader if error occurs
+      setLoading(false);
+
+      // show error
+      showMessage({
+        message: error.message,
+      });
+    }
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.form}>
-        <View style={[styles.name, styles.class]}>
-          <TextInput
-            label="Name"
-            mode="outlined"
-            style={styles.textInput}
-            autoCapitalize="none"
-            onChangeText={e => setName(e)}
-            value={name}
-          />
-        </View>
         <View style={[styles.email, styles.class]}>
           <TextInput
             label="Email"
@@ -35,6 +67,7 @@ export default function Signup({navigation}) {
           <TextInput
             label="Password"
             mode="outlined"
+            secureTextEntry
             style={styles.textInput}
             autoCapitalize="none"
             onChangeText={e => setPassword(e)}
@@ -42,11 +75,13 @@ export default function Signup({navigation}) {
           />
         </View>
         <View style={[styles.btnText, styles.class]}>
-          <Button mode="contained" onPress={() => {}} style={styles.btn}>
+          <Button mode="contained" onPress={register} style={styles.btn}>
             Sign Up
           </Button>
         </View>
       </View>
+
+      {loading && <FullLoading />}
     </View>
   );
 }
